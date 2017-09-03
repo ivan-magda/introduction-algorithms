f1e447c9f0ded8bea680f365acffede3b9ac5342
Ivan Magda <imagda15@gmail.com>
2017-09-03 21:56:10 +0300

    [foundations] refactor: Up code quality
    Up code quality for the insertion sort problem.

--- foundations/insertion-sort/InsertionSort.kt
+++ foundations/insertion-sort/InsertionSort.kt
@@ -1,18 +1,15 @@
-import java.util.*
+import java.util.Arrays
 
 fun sort(array: Array<Int>) {
-    var i = 1
-    while (i < array.size) {
+    for (i in 1 until array.size) {
         val key = array[i]
 
         var j = i - 1
         while (j >= 0 && array[j] > key) {
             array[j + 1] = array[j--]
-            j--
         }
 
         array[j + 1] = key
-        i++
     }
 }
 
